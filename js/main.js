08d6b0ea1d85caedcf3375b669525610f41d3eb2
xLazuriax <xLazuriax@users.noreply.github.com>
2019-03-11 16:34:22 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+// nbrToFind = 9;
+
+// function findNumber(){
+//    nbrGuess = document.getElementById('nbrGuess').value;
+    
+//     while(nbrGuess!=20){
+ 
+//     }if(nbrGuess<20){
+//         console.log("le nombre saisi est trop petit");
+//     }else{
+//         console.log("le nombre saisi est trop grand");
+
+//     }
+
+//     if(nbrGuess==20){
+//         console.log('Bravo, vous avez trouvé le nombre')
+//     }
+     
+//}
+
+
+// function findNumber(){
+// // Definir nombre à trouver (nbrToFind)
+// nbrToFind = 9;
+
+// // Choisir un nbrGuess
+// // Demande de chercher depuis un formulaire (champ input)
+// nbrGuess = document.getElementById('nbrGuess').value;
+
+
+
+// // si nbrToFind == nbrGuess
+// // Afficher Gagner
+
+// if(nbrToFind == nbrGuess){
+//     console.log('Vous avez gagné');
+// }else{
+
+
+//     // Si nbrToFind < nbrGuess
+//     // Afficher que le nombre est trop grand
+//     if(nbrToFind < nbrGuess){
+//         console.log('Votre nombre est trop grand');
+//     }else{
+//         console.log('Votre nombre est trop petit')
+//     }
+
+//     // Afficher nbrGuess est trop petit
+//     }
+
+// }
